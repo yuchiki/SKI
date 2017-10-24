703f6bf0b8688a131d5ecb8906d80c2d6ed63c95
Yuki <yuki08281993@gmail.com>
2017-10-24 22:38:20 +0900

    [UPDATE]

--- src/SKILibrary.hs
+++ src/SKILibrary.hs
@@ -8,14 +8,4 @@ stdlib =
 
     \let zero = k i\n\
     \let succ = s (s (k s) k) \n\
-    \let one = succ zero \n\
-    \let two = succ one \n\
-    \let three = succ two \n\
-    \let four = succ three \n\
-    \let five = succ four \n\
-    \let six = succ five \n\
-    \let seven = succ six \n\
-    \let eight = succ seven \n\
-    \let nine = succ eight \n\
-    \let ten = succ nine \n\
     \"
