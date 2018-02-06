b043ecea7e44b95ebf20e79a65bab2f3caa6846e
Yuki <yuki08281993@gmail.com>
2018-02-06 15:36:25 +0900

    add empty doctest

---
+++ test/doctest.hs
@@ -0,0 +1,9 @@
+module Main where
+
+import Build_doctests (flags, module_sources, pkgs)
+import Data.Foldable(traverse_)
+import Test.DocTest(doctest)
+
+main :: IO ()
+main = doctest args
+    where args = flags ++ pkgs ++ module_sources
